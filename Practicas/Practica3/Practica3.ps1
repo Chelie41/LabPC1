4e801e088c1a148e623a46310f0fe904523dc714
kali <marceloalerdzhdz@gmail.com>
2020-10-24 23:34:36 -0400

    agregar practica 3

---
+++ Practicas/Practica3/Practica3.ps1
@@ -0,0 +1,11 @@
+﻿function Order-ActiveProcess{
+    Get-Process | Where-Object {$_.SI -gt 0} | Sort-Object Id
+    }
+
+    $salida=0
+    Write-Host "Mostrando los procesos activos"
+    while ($salida -eq 0){
+    Order-ActiveProcess
+        $salida = Read-Host -Prompt "Desea verlos de nuevo? 1-No 0-Si"
+    }
+    Write-Host "Saliendo..."
